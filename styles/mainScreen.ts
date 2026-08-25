cb0c676225d6e0700e187c395ddb16c1df54de4d
arina.silenko <291114883+glvgvzh@users.noreply.github.com>
2026-08-25 15:28:42 +0300

    refactor: extract main screen styles

---
+++ styles/mainScreen.ts
@@ -0,0 +1,81 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  appTitle: {
+    fontSize: 28,
+    fontFamily: 'Manrope_600SemiBold',
+    marginBottom: 16,
+  },
+
+  activeQuestCard: {
+    width: '90%',
+    padding: 24,
+    borderRadius: 24,
+    backgroundColor: 'rgba(225, 225, 225, .4)',
+    overflow: 'hidden',
+  },
+  questLabel: {
+    fontSize: 14,
+    marginBottom: 8,
+    fontFamily: 'Manrope_500Medium',
+  },
+  questTitle: {
+    fontSize: 24,
+    marginBottom: 12,
+    fontFamily: 'Manrope_600SemiBold',
+  },
+  questDescription: {
+    fontSize: 16,
+    lineHeight: 22,
+    marginBottom: 20,
+    fontFamily: 'Manrope_400Regular',
+  },
+
+  completedQuestCard: {
+    width: '90%',
+    padding: 24,
+    borderRadius: 24,
+    backgroundColor: 'rgba(236, 255, 227, 0.4)',
+    overflow: 'hidden',
+  },
+
+  completedText: {
+    fontFamily: 'Manrope_600SemiBold',
+    fontSize: 18,
+    color: 'rgb(52, 116, 36)',
+  },
+
+  primaryButton: {
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    backgroundColor: 'rgba(225, 225, 225, .5)',
+    borderRadius: 16,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  primaryButtonText: {
+    fontFamily: 'Manrope_600SemiBold',
+    fontSize: 16,
+  },
+  secondaryButton: {
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    borderRadius: 16,
+    backgroundColor: 'rgba(225, 225, 225, .3)',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  secondaryButtonText: {
+    fontFamily: 'Manrope_500Medium',
+    fontSize: 16,
+  },
+});
